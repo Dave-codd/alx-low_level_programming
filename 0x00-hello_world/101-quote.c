2e672d1636757be74820a46c0ec29d02c1e817c4
Dawit Nigussie <dawit9n@gmail.com>
2022-04-07 05:38:07 -0700

    Adding 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,13 @@
 #include<unistd.h>
 /**
- * main - Entry point
+ *main - Entry point
  *
- * Return: Always 1(Sucess)
+ *Return: Always 1(Sucess)
  */
 int main(void)
-{
-	char my_str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	write(2, my_str, 60);
-	return (1);
+{ 
+	        char str0[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+		
+		write(2, str0, 60);
+  	         return (1);
 }
