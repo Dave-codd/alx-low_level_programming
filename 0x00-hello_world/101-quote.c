69b8a42e4a12a8ee4f6a4a435ebe01aa8b581e0c
Dawit Nigussie <dawit9n@gmail.com>
2022-04-07 05:06:54 -0700

    Adding 101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<stdlib.h>
+/**
+ * main - Entry point.
+ *
+ * Return: returns 1.
+ */
+int main(void)
+{
+	system("echo and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
