7ae0ec41827492229058a427781a487a7b73dc5a
Dawit Nigussie <dawit9n@gmail.com>
2022-04-08 04:19:57 -0700

    Adding 1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -5,28 +5,28 @@
  * main - Entry point
  *
  * Documentation: Generates a random number and assigns it to n.
- * then gets last digit of n and assign it to last_digit.
- * then checks if last_digit is greater than 5, it is 0 or less than 6
+ * then gets last digit of n and assign it to last.
+ * then checks if last is greater than 5, it is 0 or less than 6
  * and prints the output.
  * Return: Always 0(Success)
  */
 int main(void)
 {
 	int n;
-	int last_digit;
+	int last;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	last_digit = n % 10;
-	if (last_digit > 5)
+	last = n % 10;
+	if (last > 5)
 	{
-		printf("Last digit of %d is %d and is greater than 5\n", n, last_digit);
-	} else if (last_digit == 0)
+		printf("Last digit of %d is %d and is greater than 5\n", n, last);
+	} else if (last == 0)
 	{
-		printf("Last digit of %d is %d and is 0\n", n, last_digit);
+		printf("Last digit of %d is %d and is 0\n", n, last);
 	} else
 	{
-		printf("Last digit 0f %d is %d and is less than 6 and not 0\n", n, last_digit);
+		printf("Last digit 0f %d is %d and is less than 6 and not 0\n", n, last);
 	}
 	return (0);
 }
