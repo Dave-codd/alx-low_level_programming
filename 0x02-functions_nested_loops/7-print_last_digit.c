9af54da06fc31f483e978e8e66312bb6d01cdf95
Dawit Nigussie <dawit9n@gmail.com>
2022-04-12 13:40:08 -0700

    Adding 7-print_last_digit.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_last_digit - Prints last digit of a number
+ * @n: number to take last digit
+ *
+ * Return: lsat digit number
+ */
+int print_last_digit(int n)
+{
+	int r;
+
+	r = n % 10;
+	return (r);
+}
