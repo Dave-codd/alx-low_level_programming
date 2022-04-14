ebc048ce2be5ba8fb7bde7117cadd82542ac2efb
Dawit Nigussie <dawit9n@gmail.com>
2022-04-14 06:08:54 -0700

    Adding 9-fizz_buzz.c

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+ * main - prints numbers 1-100 with "Fizz" on multiple of 3
+ * "Buzz" on multiple of 5
+ * "FizzBuzz" on mulitple of 3 and 5
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else
+		{
+			printf("%d ", i);
+		}
+	}
+	return (0);
+}
