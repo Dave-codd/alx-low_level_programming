9c405cc0c3fdaa4add6775009f4af5288c7bb05e
Dawit Nigussie <dawit9n@gmail.com>
2022-04-14 05:46:24 -0700

    Adding 8-print_square

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_square - prints square of width and lenght size
+ * @size: size of square
+ */
+
+void print_square(int size)
+{
+	int i, j;
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size; j++)
+		{
+			_putchar(35);
+		}
+		_putchar('\n');
+	}
+	_putchar('\n');
+}
