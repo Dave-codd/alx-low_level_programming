75b71c0d111849c95bbb6dd4ab97e020336b489c
Dawit Nigussie <dawit9n@gmail.com>
2022-04-27 03:44:29 -0700

    Adding 5-sqrt_recursion.c

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * power_calculation - returns the natural square root of a number
+ * @n: input integer
+ * @c: iterator
+ *
+ * Return: square root or -1
+ */
+int power_calculation (int n, int c)
+{
+	if (c % (n / c) == 0)
+	{
+		if (c * (n / c) == n)
+			return (c);
+		else
+			return (-1);
+	}
+	return (0 + power_calculation(n, c + 1));
+}
+
+/**
+ * _sqrt_recursion - returns the natural square root of a number
+ * @n: input integer
+ *
+ * Return: natural squarw root
+ */
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+	if (n == 0)
+		return (0);
+	if (n == 1)
+		return (1);
+
+	return (power_calcalulation(n, 2));
+}
