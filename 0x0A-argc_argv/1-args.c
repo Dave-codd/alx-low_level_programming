8b2bd8d84341c4bd5ccdcb4319f2c556cc6aabee
Dawit Nigussie <dawit9n@gmail.com>
2022-04-29 03:05:28 -0700

    Adding 1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - prints number of arguments passed
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: Always 0(Sucess)
+ */
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc);
+
+	return (0);
+}
