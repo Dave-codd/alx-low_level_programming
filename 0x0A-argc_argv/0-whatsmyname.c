1762fac56a4b9e387cf09f66486eedca0bf390d6
Dawit Nigussie <dawit9n@gmail.com>
2022-04-29 02:50:57 -0700

    Adding 0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - prints the command name
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: Always 0(Sucess)
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
